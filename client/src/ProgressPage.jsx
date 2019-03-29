2bff27eecedd0512f2dac7312b15113cf337e3a1
Charles Yun <cy@Charless-MacBook-Pro.local>
2019-03-28 17:54:46 -0700

    boilerplate for pages

---
+++ client/src/ProgressPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ProgressPage extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default ProgressPage;
---
+++ client/src/TaskOptionsPage.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TaskOptionsPage extends Component {
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default TaskOptionsPage;
